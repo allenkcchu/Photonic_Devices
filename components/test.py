d069c2ed4747f6599c467aff26471d40947e680c
Allen Chu <allenchu@Urania.local>
2024-10-15 00:08:09 -0700

    Reinstall gdsfactory and ubcpdk with the latest version

--- components/test.py
+++ components/test.py
@@ -1,10 +1,12 @@
 import gdsfactory as gf
 import ubcpdk
+gf.clear_cache()
 
 @gf.cell
 def test():
     c = gf.Component()
-    c << ubcpdk.components.dbg()
+    # c << gf.components.straight(width=0.1)
+    c << ubcpdk.components.gc_te1550()
 
     return c
 
